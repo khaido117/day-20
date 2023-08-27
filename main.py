d6b9cbb9986a0bf1cae2436cabdd44a9b4199fd2
Khai Do <khaitroyy@gmail.com>
2023-08-27 12:51:27 -0500

    change variants

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-starting_x_position = [(0,0),(-20,0),(-40,0)]
+starting_position = [(0,0),(-20,0),(-40,0)]
 
-segment = []
+segments = []
 
-for snake_index in starting_x_position:
-    new_snake = Turtle(shape="square")
-    new_snake.penup()
-    new_snake.color("white")
-    new_snake.goto(snake_index)
-    segment.append(new_snake)
+for position in starting_position:
+    new_segment = Turtle(shape="square")
+    new_segment.penup()
+    new_segment.color("white")
+    new_segment.goto(position)
+    segments.append(new_segment)
     
-
 game_is_on = True 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for snake in segment:
-        snake.forward(10)
-        time.sleep(1)
+
+    for seg_num in range(len(segments)-1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 screen.exitonclick()
