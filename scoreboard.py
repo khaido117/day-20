a5d40e5fc20898d3ac53adeeb418024ce1cb272b
Khai Do <khaitroyy@gmail.com>
2023-09-02 15:34:47 -0500

    update high score function

--- scoreboard.py
+++ scoreboard.py
@@ -9,19 +9,22 @@ class Scoreboard(Turtle):
         super().__init__()
         self.goto(x=0, y=270)
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.update_scoreboard()
         self.hideturtle()
         
     def update_scoreboard(self):
-        self.write(f"Score = {self.score}", align=ALIGHMENT, font=FONT)
+        self.clear()
+        self.write(f"Score = {self.score}, High score: {self.high_score}", align=ALIGHMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over", align=ALIGHMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score 
+        self.score = 0 
+        self.update_scoreboard()
     
     def get_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
